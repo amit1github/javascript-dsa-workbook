0f48005628fb4e08b0cc5cd0fcded100d8e66c5d
Amit Kumar Yadav <49604010+amit1github@users.noreply.github.com>
2026-02-13 23:43:24 +0530

    minor

--- StriverA2Z/patterns/rightTrianglePattern.js
+++ StriverA2Z/patterns/rightTrianglePattern.js
@@ -4,7 +4,7 @@
 // ****
 
 for(let i=0; i<=3;i++) { // column
-   for(let j=0; j<=i;j++) {
+   for(let j=0; j<=i;j++) { // J should be equal or less then I.
       process.stdout.write('*')
    }
    process.stdout.write('\n')
