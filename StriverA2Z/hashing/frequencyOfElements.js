a613b3f997de05ae76d6100c6729859d899a7a77
Amit Kumar Yadav <49604010+amit1github@users.noreply.github.com>
2026-03-02 23:51:18 +0530

    Auto commit on 2/3/2026, 11:51:17 pm

---
+++ StriverA2Z/hashing/frequencyOfElements.js
@@ -0,0 +1,16 @@
+/*
+1️⃣ Count Frequency of Elements in Array
+Problem:
+Given an integer array, return a map of frequency of each element.
+
+Input:
+nums = [1, 2, 2, 3, 1, 4, 2]
+
+Output:
+{ 1: 2, 2: 3, 3: 1, 4: 1 }
+*/
+
+
+
+
+
