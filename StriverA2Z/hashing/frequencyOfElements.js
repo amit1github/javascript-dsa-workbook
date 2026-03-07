95570b2e267d987bb92d85f9d44f714260171618
Amit Yadav <samkr8991@gmail.com>
2026-03-07 22:38:40 +0530

    Auto commit on 3/7/2026, 10:38:40 PM

--- StriverA2Z/hashing/frequencyOfElements.js
+++ StriverA2Z/hashing/frequencyOfElements.js
@@ -10,7 +10,14 @@ Output:
 { 1: 2, 2: 3, 3: 1, 4: 1 }
 */
 
+function freqOfElem(params) {
+    const mapped = {}
 
+    params.forEach((item) => {
+        mapped[item] = (mapped[item] || 0) + 1;
+    });
 
+    return mapped
+}
 
-
+console.log("🚀 ~ freqOfElem:", freqOfElem([1, 2, 2, 3, 1, 4, 2]))
