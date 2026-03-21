ab5700b5e30345bbf88d8e743c7baf5275cd642b
Amit Yadav <samkr8991@gmail.com>
2026-03-22 02:39:16 +0530

    completed high and low freq: understoods this how helpful this -infinfity is

--- StriverA2Z/hashing/highestAndLowestFreqElem.js
+++ StriverA2Z/hashing/highestAndLowestFreqElem.js
@@ -10,10 +10,30 @@
 // Explanation: The frequency of 2 is 3, i.e. the highest and the frequency of 3 is 1 i.e. the lowest.
 
 const input = [10,5,10,15,10,5];
-function frequency(input) {
+function frequency(nums) {
   let map = new Map()
   
+  for(let num of nums) {
+      map.set(num, (map.get(num) || 0) + 1)
+    }
+    
+    let maxElem, minElem;
+    let maxFreq = -Infinity
+    let minFreq = Infinity
+
+    for(let [key, value] of map) {
+        if(value > maxFreq) {
+            maxFreq = value
+            maxElem = key
+        };
+
+        if (value < minFreq) {
+            minFreq = value
+            minElem = key
+        }
+    }
   
+    return [maxElem, minElem]
 }
 
 console.log(frequency(input))
