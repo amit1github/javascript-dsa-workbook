7d4b831fdaf251c125d40513bf8f87c9ada88d13
Amit Yadav <samkr8991@gmail.com>
2026-03-22 02:25:02 +0530

    Auto commit on 3/22/2026, 2:25:02 AM

---
+++ StriverA2Z/hashing/highestAndLowestFreqElem.js
@@ -0,0 +1,19 @@
+// Example 1:
+// Input: array[] = {10,5,10,15,10,5};
+// Output: 10 15
+// Explanation: The frequency of 10 is 3, i.e. the highest and the frequency of 15 is 1 i.e. the lowest.
+
+
+// Example 2:
+// Input: array[] = {2,2,3,4,4,2};
+// Output: 2 3
+// Explanation: The frequency of 2 is 3, i.e. the highest and the frequency of 3 is 1 i.e. the lowest.
+
+const input = [10,5,10,15,10,5];
+function frequency(input) {
+  let map = new Map()
+  
+  
+}
+
+console.log(frequency(input))
