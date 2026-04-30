6a8eee3756866103dc4a1f8b5d4940474b92729f
Amit Yadav <samkr8991@gmail.com>
2026-04-30 17:15:52 +0530

    withanother format

--- askedInInterviews/isValidBrackets.js
+++ askedInInterviews/isValidBrackets.js
@@ -6,31 +6,56 @@ An input string is valid if:
 3. Every close bracket has a corresponding open bracket of the same type.
  */
 
-function isValidBrackets(str) {
-    let stack = [];
-    const map = {
-        ')' : '(',
-        ']' : '[',
-        '}' : '{',
-    };
+// function isValidBrackets(str) {
+//     let stack = [];
+//     const map = {
+//         ')' : '(',
+//         ']' : '[',
+//         '}' : '{',
+//     };
 
-    for (const char of str) {
-        if (map[char] === undefined) { // checking map keys with STR inputs
-            stack.push(char)
-            console.log("🚀 ~ isValidBrackets ~ stack:", stack)
-        } else {
+//     for (const char of str) {
+//         if (map[char] === undefined) { // checking map keys with STR inputs
+//             stack.push(char)
+//             console.log("🚀 ~ isValidBrackets ~ map[char]:", map[char])
+//             console.log("🚀 ~ isValidBrackets ~ stack:", stack)
+//         } else {
 
-            console.log("🚀 ~ isValidBrackets ~ stack[stack.length-1]:", stack[stack.length-1])
-            console.log("🚀 ~ isValidBrackets ~ map[char]:", map[char])
-            if(stack.length === 0 || stack[stack.length-1] !== map[char]) {
-                return false;
-            }
+//             console.log("🚀 ~ isValidBrackets ~ stack[stack.length-1]:", stack[stack.length-1])
+//             console.log("🚀 ~ isValidBrackets ~ map[char]:", map[char])
+//             if(stack.length === 0 || stack[stack.length-1] !== map[char]) {
+//                 return false;
+//             }
 
-            stack.pop()
-        }
+//             stack.pop()
+//         }
+//     }
+
+//     return stack.length === 0
+// }
+// console.log(isValidBrackets('{[()]}'))
+
+function isValid(string) {
+  let stack = [];
+  for (let char of string) {
+    if (["(", "{", "["].includes(char)) {
+      stack.push(char);
+    } else {
+      if (stack.length === 0) return 'Invalid';
+      let last;
+      last = stack.pop();
+
+      if (
+        (char === ')' && last !== '(') ||
+        (char === '}' && last !== '{') ||
+        (char === ']' && last !== '[')
+      ) {
+        return false;
+      }
     }
+  }
 
-    return stack.length === 0
+  return stack.length === 0;
 }
 
-console.log(isValidBrackets('{[()]}'))
+console.log(isValid("[([{]})]"));
