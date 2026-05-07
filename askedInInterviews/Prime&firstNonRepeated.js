9883e9b148a7e3073d950b85ef6031b75392bcd7
Amit Yadav <samkr8991@gmail.com>
2026-05-07 20:03:22 +0530

    Auto commit on 5/7/2026, 8:03:22 PM

--- askedInInterviews/Prime&firstNonRepeated.js
+++ askedInInterviews/Prime&firstNonRepeated.js
@@ -1,24 +1,31 @@
 // Find the first non-repeated character in a string.
 // Find prime numbers between 1-100.
 
-function firstNonRepeatedChar(char) {
-    const charMap = {};
+// function firstNonRepeatedChar(char) {
+//     const charMap = {};
 
-    for(let str of char) {
-        charMap[str] = (charMap[str] || 0) + 1;
+//     for(let str of char) {
+//         charMap[str] = (charMap[str] || 0) + 1;
+//     }
+
+//     console.log(charMap)
+//     let incr = 0;
+//     for(let str of char) {
+//         if (charMap[str] === 1) {
+//             incr++;
+//             if (incr === 2) {
+//                 return str; // second non-repeating char
+//             }
+//         }
+//     }
+
+//     return false
+// }
+// console.log(firstNonRepeatedChar('aabbbcdde'))
+
+function primeNum() {
+    for (let i = 0; i < 100; i++) {
+        console.log(i)
     }
-
-    console.log(charMap)
-    let incr = 0;
-    for(let str of char) {
-        if (charMap[str] === 1) {
-            incr++;
-            if (incr === 2) {
-                return str; // second non-repeating char
-            }
-        }
-    }
-
-    return false
 }
-console.log(firstNonRepeatedChar('aabbbcdde'))
+console.log(primeNum())
